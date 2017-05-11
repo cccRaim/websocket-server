2d5ef2cf3c132d4bfb1360bf7c0bd3e69770e689
陈然 <396147081@qq.com>
2017-05-11 22:17:13 +0800

    port modified

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ config.ws.processPort.forEach((value) => {
 
 console.log(`进程已创建完毕`.blue);
 
-app.listen(config.server.port);
+app.listen(config.server.processPort[0]);
 console.log(`web服务器已经部署在localhost:${config.server.port}`.blue);
 
